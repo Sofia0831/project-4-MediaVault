f393d425743eab787dfb97b29d9746dfc6c5654c
sofiapantas <sofiasarabia0831@gmail.com>
2026-07-15 03:32:23 +0800

    setup vite and installed axios

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  // const fetchAPI = async () => {
+  //   const response = await axios.get("http://localhost:8080/api");
+  // };
+
+  // useEffect(() => {
+  //   fetchAPI();
+  // }, []);
 
   return (
     <>
