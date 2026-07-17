b0cc931620874a4174498f1ded676b93e63430c6
sofiapantas <sofiasarabia0831@gmail.com>
2026-07-17 14:31:11 +0800

    responsive navbar

--- client/src/components/HeaderNav.jsx
+++ client/src/components/HeaderNav.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import "../components/HeaderNav.css"
 
 const HeaderNav = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <header className="header">
             <div className="logo-container">
                 <span className="logo-icon">📚</span>
                 <h1 className="logo-text">MediaVault</h1>
             </div>
+            
+            <button 
+                className={`hamburger ${isOpen ? "open" : ""}`}
+                onClick={toggleMenu}
+                aria-label="Toggle navigation menu"
+            >
+                <span className="line"></span>
+                <span className="line"></span>
+                <span className="line"></span>
+            </button>
 
-            <nav className="nav-bar">
+            <nav className={`nav-bar ${isOpen ? "show": ""}`}>
                 <button className="nav-button">Home</button>
                 <button className="nav-button">Shelf</button>
                 <button className="nav-button">Profile</button>
