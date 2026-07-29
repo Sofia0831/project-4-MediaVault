8f6b0ef3303742b4ce5d72b23aa5d03b0962f425
sofiapantas <sofiasarabia0831@gmail.com>
2026-07-30 03:05:24 +0800

    adjusted api services

--- server/src/controllers/movieController.js
+++ server/src/controllers/movieController.js
@@ -3,109 +3,83 @@ import tmdbService from "../services/tmdbService.js";
 const movieController = {};
 
 movieController.searchMovies = async (req, res) => {
-    try {
+  try {
+    const { query, page } = req.query;
 
-        const { query } = req.query;
-
-        if (!query) {
-            return res.status(400).json({
-                message: "Search query required."
-            });
-        }
-
-        const movies = await tmdbService.searchMovies(query);
-
-        res.json(movies);
-
-    } catch (error) {
-
-        res.status(500).json({
-            message: error.message
-        });
-
+    if (!query) {
+      return res.status(400).json({
+        message: "Search query required.",
+      });
     }
+
+    const movies = await tmdbService.searchMovies(query, page);
+
+    res.json(movies);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 
 movieController.getMovie = async (req, res) => {
+  try {
+    const movie = await tmdbService.getMovie(req.params.id);
 
-    try {
-
-        const movie = await tmdbService.getMovie(req.params.id);
-
-        res.json(movie);
-
-    } catch (error) {
-
-        res.status(500).json({
-            message: error.message
-        });
-
-    }
-
+    res.json(movie);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 
 movieController.getTrending = async (req, res) => {
+  try {
+    const { page } = req.query;
 
-    try {
+    const movies = await tmdbService.getTrendingMovies(page);
 
-        const movies = await tmdbService.getTrendingMovies();
-
-        res.json(movies);
-
-    } catch (error) {
-
-        res.status(500).json({
-            message: error.message
-        });
-
-    }
-
+    res.json(movies);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 
 movieController.getPopular = async (req, res) => {
+  try {
+    const { page } = req.query;
 
-    try {
+    const movies = await tmdbService.getPopularMovies(page);
 
-        const movies = await tmdbService.getPopularMovies();
-
-        res.json(movies);
-
-    } catch (error) {
-
-        res.status(500).json({
-            message: error.message
-        });
-
-    }
-
+    res.json(movies);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 
 movieController.getRecommendations = async (req, res) => {
+  try {
+    const { id } = req.query;
 
-    try {
-
-        const { id } = req.query;
-
-        if (!id) {
-
-            return res.status(400).json({
-                message: "Movie id required."
-            });
-
-        }
-
-        const movies = await tmdbService.getMovieRecommendations(id);
-
-        res.json(movies);
-
-    } catch (error) {
-
-        res.status(500).json({
-            message: error.message
-        });
-
+    if (!id) {
+      return res.status(400).json({
+        message: "Movie id required.",
+      });
     }
 
+    const movies = await tmdbService.getMovieRecommendations(id);
+
+    res.json(movies);
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 
 export default movieController;
